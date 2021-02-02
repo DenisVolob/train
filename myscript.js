2eee0a4ec4eb4091d330937102ae404f39afa005
DenisVolob <78219474+DenisVolob@users.noreply.github.com>
2021-02-02 10:06:42 -0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,17 @@
+/* Элемент меняет цвет при наведении на него */
+button.yt-icon-button:hover {
+    vertical-align: middle;
+    color: #ff0087;
+}
+
+/* Элемент меняет цвет во время нажатия на доли секунд.  */
+button.yt-icon-button:active {
+    vertical-align: middle;
+    color: #01a715;
+
+
+/* Элемент меняет цвет после нажатия, но стоит потом куда-то кликнуть и изменённый цвет заменяется на цвет по умолчанию. */
+button.yt-icon-button:focus {
+    vertical-align: middle;
+    color: #01a715;
+}
